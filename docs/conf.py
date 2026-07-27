5e22c1ae8e9b6dc9a3e71a89b454529896091aae
YongxinFred <43864416+YongxinFred@users.noreply.github.com>
2026-07-27 10:54:08 -0600

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+# Minimal Sphinx configuration for CONUS404-future
+project = 'CONUS404-future'
+copyright = '2026, NCAR'
+author = 'NCAR'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
